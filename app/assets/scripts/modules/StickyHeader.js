191a96b96702a17bef1bac2a12880cec75792159
Sean <sean.nguyen.001@gmail.com>
2018-03-16 09:42:00 -0700

    Completed the Modal overlay.

--- app/assets/scripts/modules/StickyHeader.js
+++ app/assets/scripts/modules/StickyHeader.js
@@ -15,7 +15,7 @@ class StickyHeader {
 
   addSmoothScrolling() {
     this.headerLinks.smoothScroll();
-  }
+  } 
 
   createHeaderWaypoint() {
     var that = this;
---
+++ app/markup-viewer.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import $ from 'jquery';
+
+class DisplayContainer extends React.component {
+	constructor(props) {
+		super(props);
+	}
+	render() {
+
+	}
+}
+
+class RawInput extends React.component {
+	constructor(props) {
+		super(props);
+	}
+	render() {
+		return (
+			
+		);
+	}
+	
+}
